63510d760396ea62ec541caf0d4c32dc40f5a701
Arthur <a-murk@mail.ee>
2022-06-27 23:12:43 +0300

    Add global projects constant and import create project card module

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import './styles/main.scss';
 import { projectCreation } from './modules/projectCreation';
+import { createProjectCard } from './modules/createProjectCard';
 
+const projects = [];
 const projCreate = projectCreation;
