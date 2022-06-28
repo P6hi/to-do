ea179013d3394f7fb25a96b2aa8a51a5d597aec5
Arthur <a-murk@mail.ee>
2022-06-28 15:36:51 +0300

    Add project header template

--- src/modules/createProjectCard.js
+++ src/modules/createProjectCard.js
@@ -4,7 +4,7 @@ import { removeProject } from "./removeProject";
 export function createProjectCard(projectList, project, index) {
     const main = document.querySelector('main');
     const container = elementCreator('div', 'project-container');
-    const projectHeader = elementCreator('h2', 'project-header', project.title);
+    const projectHeader = elementCreator('h2', 'project-header', `Project: ${project.title}`);
     const removeBtn = elementCreator('button', 'remove-btn', 'Delete Project');
     const addTaskBtn = elementCreator('button', 'add-btn', 'Add a task');
     container.append(projectHeader);
