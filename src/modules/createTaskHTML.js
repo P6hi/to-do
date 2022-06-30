9e9756283e8fb3193214264b4beea2c5644be8a9
Arthur <a-murk@mail.ee>
2022-06-30 16:04:08 +0300

    Convert task title to a h3 element

--- src/modules/createTaskHTML.js
+++ src/modules/createTaskHTML.js
@@ -3,15 +3,17 @@ import { removeTask } from "./removeTask";
 
 export function createTaskHTML(projContainer, project, task) {
     const taskContainer = elementCreator('div', 'task-container');
-    const titleHeader = elementCreator('ul', 'task-header', task.title);
+    const taskList = elementCreator('ul', 'task-header');
+    const taskTitle = elementCreator('h3', 'task-header', task.title);
     const desc = elementCreator('li', 'task-desc', task.description);
     const dueDate = elementCreator('li', 'task-due-date', task.dueDate);
     const priority = elementCreator('li', 'task-priority', task.priority);
     const removeBtn = elementCreator('button', 'task-remove-btn', 'Remove Task');
-    taskContainer.append(titleHeader);
-    titleHeader.append(desc);
-    titleHeader.append(dueDate);
-    titleHeader.append(priority);
+    taskContainer.append(taskList);
+    taskList.append(taskTitle);
+    taskList.append(desc);
+    taskList.append(dueDate);
+    taskList.append(priority);
     taskContainer.append(removeBtn);
     projContainer.append(taskContainer);
     removeBtn.addEventListener('click', () => {
